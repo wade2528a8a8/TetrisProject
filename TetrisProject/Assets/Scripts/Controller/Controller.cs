8e70aa7985181dcd5e9d1d95dd01409983b5589d
Wade Chuang <wade2528a8a8@gmail.com>
2018-12-09 16:00:42 +0800

    Load TetrisProject

---
+++ TetrisProject/Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [HideInInspector]
+    public Model model;
+    [HideInInspector]
+    public View view;
+
+
+    private void Awake()
+    {
+        model = GameObject.FindGameObjectWithTag(Tags.Model).GetComponent<Model>();
+        view = GameObject.FindGameObjectWithTag(Tags.View).GetComponent<View>();
+
+
+
+    }
+
+
+}
---
+++ TetrisProject/Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags : MonoBehaviour {
+
+    public const string Model = "Model";
+    public const string View = "View";
+
+
+}
